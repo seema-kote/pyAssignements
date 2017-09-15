279020d946e3fb5245edaf4e0e3d1744fc7ee4ab
Seema Kote <31430773+seema-kote@users.noreply.github.com>
2017-09-14 23:06:05 -0700

    Update Bank_transaction_Example.py

--- Bank_transaction_Example.py
+++ Bank_transaction_Example.py
@@ -6,7 +6,7 @@
 	Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
 	D 100
 	W 200
-	¡­
+	Â¡Â­
 	D means deposit while W means withdrawal.
 	Suppose the following input is supplied to the program:
 	D 300
@@ -16,6 +16,8 @@
 	Then, the output should be:
 	500
 """
+# Answer : 
+
 print "Transacition Details: "
 netAmount = 0
 while True:
